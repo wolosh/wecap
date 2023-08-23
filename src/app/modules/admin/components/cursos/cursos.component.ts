804e5d8212f04f3c2bdd14647545eb0fc75de316
gladisjrm <gladisjrm31@gmail.com>
2023-08-23 15:58:10 -0600

    cursos

--- src/app/modules/admin/components/cursos/cursos.component.ts
+++ src/app/modules/admin/components/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GetService } from 'src/app/data/services/get.service';
+import { HelpersService } from 'src/app/data/services/helpers.service';
 
 @Component({
   selector: 'app-cursos',
@@ -6,11 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  certificaciones: any;
+  email: any;
 
-  constructor() { }
+  constructor(private get: GetService, public helpers: HelpersService) { }
 
   ngOnInit(): void {
+    console.log(localStorage.getItem('token'));
+    this.profile();
   }
 
-
+  profile(){
+    this.get.getProfile(localStorage.getItem('id'), localStorage.getItem('token')).subscribe(
+      (data: any) => {
+        //console.log(data);
+        this.email = data;
+        //console.log(this.email);
+        this.certifications(data.email);
+      }
+    );
+  }
+    
+  certifications(email:any){
+    this.certificaciones = [];
+    this.get.getCertifications(email, localStorage.getItem('token')).subscribe(
+      (data: any) => {
+        //console.log(data);
+        this.certificaciones = data;
+        console.log(this.certificaciones);
+      }
+    );
+  }
 }
