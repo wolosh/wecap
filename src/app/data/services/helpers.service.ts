b13d0604fa541f4343ea45cb6375048b3f4ddb6b
alejandra <alexoa97@hotmail.com>
2024-05-08 16:01:03 -0600

    corrección helpers url

--- src/app/data/services/helpers.service.ts
+++ src/app/data/services/helpers.service.ts
@@ -38,16 +38,16 @@ export class HelpersService {
 
   constructor(private route: Router, public session:SessionService) {
     if (this.domainPrueba.includes('americargo')) {
-      this.domain = 'https://ci.americargo.wecap.mx/api/'
+      this.domain = 'https://ci.americargo.wecap.mx/'
       console.log(this.domain)
       } else if (this.domainPrueba.includes('alsainacademy')) {
-        this.domain = 'https://ci.alsainacademy.wecap.mx/api/'
+        this.domain = 'https://ci.alsainacademy.wecap.mx/'
         console.log(this.domain)
       } else if(this.domainPrueba.includes('unitrade')) {
-        this.domain = 'https://ci.unitrade.wecap.mx/api/'
+        this.domain = 'https://ci.unitrade.wecap.mx/'
         console.log(this.domain)
       } else {
-        this.domain = 'https://ci.wecap.mx/api/'
+        this.domain = 'https://ci.wecap.mx/'
         console.log(this.domain)
       }
    }
